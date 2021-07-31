b485406217fcf7c859d4d0d621b89269b5a4184e
Dustin Crider <the_cridder@hotmail.com>
2021-07-31 12:54:49 -0600

    end of sec 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../models/pagination';
 import { IType } from '../models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../models/shopParams';
+import { IProduct } from '../models/product';
 
 @Injectable({
   providedIn: 'root' //aka app.module.ts
@@ -39,6 +40,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
